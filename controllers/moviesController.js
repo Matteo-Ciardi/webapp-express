31290b44a2f5c1a9235e7333f622210dca932e0f
MatteoCiardi <ciardimatteo99@gmail.com>
2025-10-30 16:39:22 +0100

    aggiunto movie controller e vari middlewares

---
+++ controllers/moviesController.js
@@ -0,0 +1,37 @@
+const connection = require('../data/db');
+
+function index(req, res) {
+
+    const sql = 'SELECT * FROM movies';
+
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        res.json(results);
+    });
+}
+
+
+function show(req, res) {
+    const id = req.params.id
+
+    const movieSql = 'SELECT * FROM movies WHERE id = ?';
+
+    const reviewsSql = 'SELECT * FROM reviews WHERE movie_id = ?';
+
+    connection.query(movieSql, [id], (err, movieResults) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (movieResults.length === 0) return res.status(404).json({ error: 'Movie not found' });
+
+        const movie = movieResults[0];
+
+        connection.query(reviewsSql, [id], (err, reviewsResults) => {
+            if (err) return res.status(500).json({ error: 'Database query failed' });
+
+            movie.reviews = reviewsResults;
+            res.json(movie);
+        });
+    });
+
+}
+
+module.exports = { index, show }
